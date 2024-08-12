ad30497f538116b83194dceb4a8c18156b1a4a7a
marius <marius.ahsmus@gmail.com>
2024-08-12 12:40:55 +0200

    try again for publish

--- src/components/views/AboutView.tsx
+++ src/components/views/AboutView.tsx
@@ -3,7 +3,6 @@
 import React, {HTMLAttributes} from "react";
 import {motion} from "framer-motion";
 
-
 export const AboutView: React.FC<HTMLAttributes<HTMLDivElement>> = ({}) => {
 
     return (
@@ -11,7 +10,7 @@ export const AboutView: React.FC<HTMLAttributes<HTMLDivElement>> = ({}) => {
             initial={{opacity: 0, filter: 'blur(10px)', y: 50}}
             animate={{opacity: 1, filter: 'blur(0px)', y: 0}}
             transition={{duration: 0.65}}
-            className="flex md:flex-row md:space-x-16 md:space-y-0 flex-col space-y-8 justify-between"
+            className={"flex md:flex-row md:space-x-16 md:space-y-0 flex-col space-y-8 justify-between"}
         >
             <p className={"text-zinc-800 text-sm max-w-[70%] leading-8 text-pretty"}>
                 Hello! My name is Marius, and I am a dedicated software developer with a passion for creating innovative and efficient solutions. With a strong foundation in Java and C#, I have developed a keen ability to turn complex problems into streamlined, user-friendly applications. Currently, I am pursuing my bachelor's degree, with an expected graduation in 2026. In my free time, I have recently delved into frontend programming with TypeScript, Next.js, and Tailwind CSS.
